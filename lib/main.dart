93b569b27fe8878e21b1e07c1b1cf463b078d7e9
Thullio_Ferreira <thullioferreira6@gmail.com>
2025-04-23 00:39:34 +0000

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,34 @@
+//Firebase
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+//Material UI
 import 'package:flutter/material.dart';
+//Páginas Internas
+import 'firebase_options.dart';
+import 'pages/login_page.dart';
+import 'pages/pokedex.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyCPWZ-xbasRaDExcUvYNb2-I1zPBcKizYg",
+        authDomain: "pokedex-app-12f28.firebaseapp.com",
+        projectId: "pokedex-app-12f28",
+        storageBucket: "pokedex-app-12f28.firebasestorage.app",
+        messagingSenderId: "147417047798",
+        appId: "1:147417047798:web:815713eb19374ac7da02d7"),
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
